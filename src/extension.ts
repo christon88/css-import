04677d8ee6b836ff49e4c0e21da6b616cfb4c7fa
Christian Lærum-Onsager <christian.larum-onsa@visma.com>
2024-02-01 11:10:18 +0100

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,153 +1,64 @@
 import path from "path";
 import * as vscode from "vscode";
 import * as fs from "fs";
-import postcss from "postcss";
+import { getClassNames, getVariables } from "./shared";
 
 export function activate(context: vscode.ExtensionContext) {
-  const classNameProvider = vscode.languages.registerCompletionItemProvider(
-    "typescriptreact",
-    {
-      async provideCompletionItems(
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        document: vscode.TextDocument,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        position: vscode.Position
-      ) {
-        const cssUtilityPath = path.join(
-          __dirname,
-          "..",
-          "node_modules",
-          "@tlx/atlas",
-          "dist",
-          "atlas-utilities.css"
-        );
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (workspaceFolders && workspaceFolders.length > 0) {
+    const workspaceRootPath = workspaceFolders[0].uri.fsPath; // Assuming single workspace
+    const configPath = path.join(workspaceRootPath, ".atlAutocomplete.json");
 
-        const getClassNames = async () => {
-          const cssContent = await readCssFile(cssUtilityPath);
-
-          const classRules = extractClassNamesAndRules(cssContent);
-
-          const completionItems = [];
-
-          for (const [className, rules] of classRules.entries()) {
-            const item = new vscode.CompletionItem(
-              className,
-              vscode.CompletionItemKind.Class
-            );
-            //   const documentation =new vscode.MarkdownString('```css\n' + rules + '\n```'); // Use MarkdownString for better formatting
-            item.detail = rules;
-            completionItems.push(item);
-          }
-
-          return completionItems;
-        };
-
-        const completionItems = await getClassNames();
-        return completionItems;
-      },
-    }
-  );
-
-  const variableProvider = vscode.languages.registerCompletionItemProvider(
-    "css",
-    {
-      async provideCompletionItems(
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        document: vscode.TextDocument,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        position: vscode.Position
-      ) {
-        const cssVariablesPath = path.join(
-          __dirname,
-          "..",
-          "node_modules",
-          "@tlx/atlas",
-          "dist",
-          "atlas-base.css"
-        );
-
-        const getVariables = async () => {
-          const cssContent = await readCssFile(cssVariablesPath);
-
-          const cssVariables = extractCssVariables(cssContent);
-
-          const completionItems = Object.entries(cssVariables).map(
-            ([variableName, variableValue]) => {
-              const completionItem = new vscode.CompletionItem(
-                variableName,
-                vscode.CompletionItemKind.Variable
-              );
-              completionItem.detail = variableValue; // Optionally, show the variable value in the detail
-              return completionItem;
-            }
-          );
-          return completionItems;
-        };
-
-        const completionItems = await getVariables();
-        return completionItems;
-      },
-    }
-  );
-
-  context.subscriptions.push(classNameProvider);
-  context.subscriptions.push(variableProvider);
-}
-
-function readCssFile(filePath: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf8", (err, data) => {
+    fs.readFile(configPath, "utf8", async (err, data) => {
       if (err) {
-        reject(err);
+        vscode.window.showErrorMessage(
+          ".atlAutocomplete file not found or unreadable."
+        );
         return;
       }
-      resolve(data);
-    });
-  });
-}
+      const config: { cssUtilityPath: string; cssVariablesPath: string } =
+        JSON.parse(data);
+      // Now you have your configuration object
+      const { cssUtilityPath, cssVariablesPath } = config;
 
-function extractClassNamesAndRules(cssContent: string) {
-  const classRules = new Map<string, string>(); // Use a Map to store class names and their rules
-
-  // Use PostCSS to parse the CSS
-  const root = postcss.parse(cssContent);
-
-  // Walk through all the rules
-  root.walkRules((rule) => {
-    rule.selectors.forEach((selector) => {
-      // Match class selectors
-      // eslint-disable-next-line no-useless-escape
-      const classRegex = /\.([^\s\.#:\[]+)/g;
-      let match;
-      while ((match = classRegex.exec(selector)) !== null) {
-        const className = match[1]; // Extract class name
-        if (!classRules.has(className)) {
-          classRules.set(className, rule.toString()); // Store the whole rule as a string
+      const classCompletionItems = await getClassNames(cssUtilityPath);
+      const classNameProvider = vscode.languages.registerCompletionItemProvider(
+        "typescriptreact",
+        {
+          async provideCompletionItems(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            document: vscode.TextDocument,
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            position: vscode.Position
+          ) {
+            return classCompletionItems;
+          },
         }
-      }
+      );
+      const variableCompletionItems = await getVariables(cssVariablesPath);
+      const variableProvider = vscode.languages.registerCompletionItemProvider(
+        "css",
+        {
+          async provideCompletionItems(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            document: vscode.TextDocument,
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            position: vscode.Position
+          ) {
+            return variableCompletionItems;
+          },
+        }
+      );
+
+      context.subscriptions.push(classNameProvider);
+      context.subscriptions.push(variableProvider);
     });
-  });
-
-  return classRules; // Return the Map containing class names and their rules
+  } else {
+    vscode.window.showErrorMessage("No workspace found.");
+  }
 }
 
-function extractCssVariables(cssContent: string) {
-  const cssVariables = new Map<string, string>();
-
-  // Use PostCSS to parse the CSS
-  const root = postcss.parse(cssContent);
-
-  // Walk through all the declarations to find CSS variables
-  root.walkDecls((decl) => {
-    if (decl.prop.startsWith("--")) {
-      // Store variable name and value
-      cssVariables.set(decl.prop, decl.value);
-    }
-  });
-
-  return Object.fromEntries(cssVariables);
-}
-
-export function deactivate() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function deactivate() {
   // Nothing specific to clean up in this case
 }
---
+++ src/shared.ts
@@ -0,0 +1,95 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import postcss from "postcss";
+
+export const getClassNames = async (path: string) => {
+  const cssContent = await readCssFile(path);
+  console.log("here");
+  const classRules = extractClassNamesAndRules(cssContent);
+
+  const completionItems = [];
+
+  for (const [className, rules] of classRules.entries()) {
+    const item = new vscode.CompletionItem(
+      className,
+      vscode.CompletionItemKind.Class
+    );
+    //   const documentation =new vscode.MarkdownString('```css\n' + rules + '\n```'); // Use MarkdownString for better formatting
+    item.detail = rules;
+    completionItems.push(item);
+  }
+
+  return completionItems;
+};
+
+export const getVariables = async (path: string) => {
+  const cssContent = await readCssFile(path);
+
+  const cssVariables = extractCssVariables(cssContent);
+
+  const completionItems = Object.entries(cssVariables).map(
+    ([variableName, variableValue]) => {
+      const completionItem = new vscode.CompletionItem(
+        variableName,
+        vscode.CompletionItemKind.Variable
+      );
+      completionItem.detail = variableValue; // Optionally, show the variable value in the detail
+      return completionItem;
+    }
+  );
+  return completionItems;
+};
+
+function extractClassNamesAndRules(cssContent: string) {
+  const classRules = new Map<string, string>(); // Use a Map to store class names and their rules
+
+  // Use PostCSS to parse the CSS
+  const root = postcss.parse(cssContent);
+
+  // Walk through all the rules
+  root.walkRules((rule) => {
+    rule.selectors.forEach((selector) => {
+      // Match class selectors
+      // eslint-disable-next-line no-useless-escape
+      const classRegex = /\.([^\s\.#:\[]+)/g;
+      let match;
+      while ((match = classRegex.exec(selector)) !== null) {
+        const className = match[1]; // Extract class name
+        if (!classRules.has(className)) {
+          classRules.set(className, rule.toString()); // Store the whole rule as a string
+        }
+      }
+    });
+  });
+
+  return classRules; // Return the Map containing class names and their rules
+}
+
+function extractCssVariables(cssContent: string) {
+  const cssVariables = new Map<string, string>();
+
+  // Use PostCSS to parse the CSS
+  const root = postcss.parse(cssContent);
+
+  // Walk through all the declarations to find CSS variables
+  root.walkDecls((decl) => {
+    if (decl.prop.startsWith("--")) {
+      // Store variable name and value
+      cssVariables.set(decl.prop, decl.value);
+    }
+  });
+
+  return Object.fromEntries(cssVariables);
+}
+
+function readCssFile(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
